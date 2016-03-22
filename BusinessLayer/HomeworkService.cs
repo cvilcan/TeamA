b571f26ab2f3b37d96bccb5bb5db5a80418cf0b3
Alina Craciun <alina.craciun@softvision.ro>
2016-03-22 14:58:36 +0200

    Create Homework View

---
+++ BusinessLayer/HomeworkService.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class HomeworkService
+    {
+        HomeworkRepository hmwrkService;
+    }
+}
---
+++ TeamA/Models/HomeworkVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TeamA.Models
+{
+    public class HomeworkVM
+    {
+        [DisplayName("Problem Description")]
+        public string Description { get; set; }
+        
+        public DateTime Deadline { get; set; }
+    }
+}
