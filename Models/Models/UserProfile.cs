34591bf4839a4fcc5bb307d4399c9dc73dc3cfd2
rares.banc <rares.banc@softvision.ro>
2016-03-22 16:28:15 +0200

    l

--- Models/Models/UserProfile.cs
+++ Models/Models/UserProfile.cs
@@ -12,6 +12,6 @@ namespace AccessModels.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public int RoleID { get; set; }
+        public string RoleName { get; set; }
     }
 }
