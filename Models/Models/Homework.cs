468ab0689ea0f8ed693cdf428831f98031899068
Gabriel Cucerzan <gabriel.cucerzan@softvision.ro>
2016-03-22 11:13:44 +0200

    am adaugat ceva

---
+++ Models/Models/Homework.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    class Homework
+    {
+        public int HomeworkID { get; set; }
+        public int TeacherUserID { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
