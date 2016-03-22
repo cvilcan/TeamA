2ebb5f49c6a540ff44503cb217e5489ab77be132
Gabriel Cucerzan <gabriel.cucerzan@softvision.ro>
2016-03-22 15:13:42 +0200

    models models

---
+++ Models/Models/StudentsToTeachers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    class StudentsToTeachers
+    {
+        public int StudentID { get; set; }
+        public int TeacherID { get; set; } 
+    }
+}
