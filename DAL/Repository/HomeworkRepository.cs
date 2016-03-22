cf2a6f2b0cbe7e6691bedcc0cd68824e00348294
Gabriel Cucerzan <gabriel.cucerzan@softvision.ro>
2016-03-22 17:49:52 +0200

    HomeR

--- DAL/Repository/HomeworkRepository.cs
+++ DAL/Repository/HomeworkRepository.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using AccessModels.Models;
 namespace DAL.Repository
 {
     public class HomeworkRepository
     {
+        string cs = ConfigurationManager.ConnectionStrings["TeamAConnection"].ConnectionString;
+        
+        public void CreateHomework(string teacherID,string description, DateTime deadline)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spCreateHomework");
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@teacherID", teacherID);
+                cmd.Parameters.AddWithValue("@homeWorkDescription", description);
+                cmd.Parameters.AddWithValue("@deadlineInDays", deadline);
+                
+
+                cmd.ExecuteNonQuery();
+
+            }
+        }
     }
 }
