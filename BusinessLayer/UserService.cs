e7730c4a42d8ea2afd13e5e7a547da029635dd69
Vilcan Ciprian <ciprian.vilcan@softvision.ro>
2016-03-24 13:09:05 +0200

    Fixed more git BS text from merges

--- BusinessLayer/UserService.cs
+++ BusinessLayer/UserService.cs
@@ -14,7 +14,7 @@ namespace BusinessLayer
     public class UserService
     {
         private UserRepository _userRepository = new UserRepository();
-        
+
         public IEnumerable<UserProfile> GetAllUsers()
         {
             try
@@ -81,8 +81,10 @@ namespace BusinessLayer
             else
                 return false;
         }
-        public string GetRole(string username)        {
-            var role =_userRepository.GetRole(username);
+        public string GetRole(string username)
+        {
+            var role = _userRepository.GetRole(username);
             return role;
         }
     }
+}
