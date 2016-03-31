26a0674b506c39d0cd9dd64bd7ef41b0d5dbdff2
Vilcan Ciprian <ciprian.vilcan@softvision.ro>
2016-03-31 09:54:46 +0300

    Fixed completed homework

---
+++ BusinessLayer/Models/MyDateAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models
+{
+    public class MyDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime d = Convert.ToDateTime(value);
+            return d > DateTime.Now;
+
+        }
+    }
+}
